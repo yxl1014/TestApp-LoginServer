ddb79cf467c5893675474910ddc6e9470f158dbe
yxl1014 <1627419257@qq.com>
2022-08-30 10:36:33 +0800

    init package and pom.xml

---
+++ src/main/java/yxl/testapp/LoginApplication.java
@@ -0,0 +1,15 @@
+package yxl.testapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yxl
+ * @date:  2022/8/30
+ */
+@SpringBootApplication
+public class LoginApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LoginApplication.class, args);
+    }
+}
