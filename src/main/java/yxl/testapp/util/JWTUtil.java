7bba7257cd057bc9ed727be299464130dd8db31e
yxl1014 <1627419257@qq.com>
2022-09-13 16:43:34 +0800

    TestApp_V2 JWTUtil

--- src/main/java/yxl/testapp/util/JWTUtil.java
+++ src/main/java/yxl/testapp/util/JWTUtil.java
@@ -1,67 +1,74 @@
 package yxl.testapp.util;
 
 
-import com.auth0.jwt.JWTSigner;
+import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.internal.com.fasterxml.jackson.databind.ObjectMapper;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.google.protobuf.InvalidProtocolBufferException;
 import yxl.testapp.domain.TestProto;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 
+/**
+ * token加密解密类
+ */
 public class JWTUtil {
 
-    private static final String SECRET = "!=end=!";
+    private static final String ISSUE = "YXL";
 
-    private static final String PAYLOAD = "payload";
-
-    private static final String EXP = "exp";
+    private static final String SECRET = "011014";
 
 
     //加密，传入一个对象和有效期
     public static <T> String sign(T object, long maxAge) {
-        try {
-            final JWTSigner signer = new JWTSigner(SECRET);
-            final Map<String, Object> claims = new HashMap<>();
-            //通过jackson的api将对象转换成json
-            ObjectMapper mapper = new ObjectMapper();
-            String jsonString = mapper.writeValueAsString(object);
-            claims.put(PAYLOAD, jsonString);
-            claims.put(EXP, System.currentTimeMillis() + maxAge);
-            return signer.sign(claims);
-        } catch (Exception e) {
-            return null;
-        }
+        Map<String, Object> claims = new HashMap<>();
+        long l = System.currentTimeMillis();
+        claims.put("USER", object);
+
+        return JWT.create()
+                .withIssuer(ISSUE)
+                .withIssuedAt(new Date(l))
+                .withHeader(claims)
+                .withExpiresAt(new Date(l + maxAge))
+                .sign(Algorithm.HMAC384(SECRET));
     }
 
 
     //解密，传入一个加密后的token字符串和解密后的类型
-    public static <T> T unsign(String jwt, Class<T> classT) {
-        final JWTVerifier verifier = new JWTVerifier(SECRET);
+    public static <T> T unsign(String token, Class<T> classT) {
+        DecodedJWT jwt;
         try {
-            final Map<String, Object> claims = verifier.verify(jwt);
-            if (claims.containsKey(EXP) && claims.containsKey(PAYLOAD)) {
-                long exp = (Long) claims.get(EXP);
-                long currentTimeMillis = System.currentTimeMillis();
-                if (exp > currentTimeMillis) {
-                    String json = (String) claims.get(PAYLOAD);
-                    ObjectMapper objectMapper = new ObjectMapper();
-                    return objectMapper.readValue(json, classT);
-                }
-            }
-            return null;
+            JWTVerifier verifier = JWT.require(Algorithm.HMAC384(SECRET))
+                    .withIssuer(ISSUE)
+                    .build();
+            jwt = verifier.verify(token);
+            return jwt.getHeaderClaim("USER").as(classT);
         } catch (Exception e) {
             return null;
         }
     }
 
-    public static void main(String[] args) {
-        //TODO 得改
+    public static void main(String[] args) throws InvalidProtocolBufferException, InterruptedException {
         TestProto.User.Builder builder = TestProto.User.newBuilder();
-        builder.setUserPassword("123456");
-        TestProto.User user = builder.build();
-        System.out.println(JWTUtil.sign(user, 3000));
+        byte[] bytes = builder.setUserPassword("123456").build().toByteArray();
+
+        String token = JWTUtil.sign(bytes, 3000);
+
+        byte[] data = JWTUtil.unsign(token, byte[].class);
+        TestProto.User user = TestProto.User.parseFrom(data);
+        System.out.println(user.getUserPassword());
+        Thread.sleep(5000);
+        byte[] data2 = JWTUtil.unsign(token, byte[].class);
+        if (data2 == null) {
+            return;
+        }
+        TestProto.User user1 = TestProto.User.parseFrom(data2);
+        System.out.println(user1.getUserPassword());
+
     }
 
 }
